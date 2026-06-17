fd3b451a5fdb1734adeb33eb590f81bc35a60fc3
unknown <universobrother2@gmail.com>
2026-06-17 20:04:49 -0300

    [FEATURE(UPDATE)] Music Implemented and some little adjusment

--- Assets/MyProject/Script/UI UX  scripts/ChangeScene.cs
+++ Assets/MyProject/Script/UI UX  scripts/ChangeScene.cs
@@ -9,10 +9,21 @@ public class ChangeScene : MonoBehaviour
     [SerializeField] float fadeDuration = 1f;
     [SerializeField] string nextScene;
 
+    [SerializeField] AudioSource audioSource;
+    float initialVolume;
+
     void Awake()
     {
         gameObject.SetActive(false);
+
+        audioSource = FindAnyObjectByType<AudioSource>();
+
+        if (audioSource != null)
+        {
+            initialVolume = audioSource.volume;
+        }
     }
+
     public void StartFade()
     {
         gameObject.SetActive(true);
@@ -25,10 +36,22 @@ public class ChangeScene : MonoBehaviour
 
         while (color.a < 1)
         {
+            float progress = color.a;
+
             color.a += Time.deltaTime / fadeDuration;
             fadeImage.color = color;
 
+            if (audioSource != null)
+            {
+                audioSource.volume = Mathf.Lerp(initialVolume, 0f, progress);
+            }
+
             yield return null;
+        }
+
+        if (audioSource != null)
+        {
+            audioSource.volume = 0f;
         }
 
         SceneManager.LoadScene(nextScene);
